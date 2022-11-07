2b9c41b269af52a7c00e86f06e08250e72b91663
Biurons <ifeoma.umenzeakor@gmail.com>
2022-11-07 01:40:21 -0800

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *s)
 {
 	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letter[] = {65, 69, 79, 84, 76};
+	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
@@ -18,7 +18,7 @@ char *leet(char *s)
 		{
 			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
 			{
-				*(s + count) = number[i];
+				*(s + count) = numbers[i];
 				break;
 			}
 		}
